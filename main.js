c4a70efb3a9b8f62b403e6921fcc71ddec7ec69d
Nandanakrishna03 <nandanakrishna75@gmail.com>
2024-09-05 14:18:41 +0530

    index.html

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
     
         // JavaScript to display even numbers
-        const evenNumbersContainer = document.getElementById('even-numbers');
+        let evenNumbersContainer = document.getElementById('even-numbers');
 
         for (let i = 0; i <= 10; i++) {
             if (i % 2 === 0) {
-                const numberElement = document.createElement('p');
-                numberElement.textContent = i;
+                let numberElement = document.createElement('p');
+                numberElement.innerText = i;
                 evenNumbersContainer.appendChild(numberElement);
                 console.log(i)
             }
